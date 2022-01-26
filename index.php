f577f43dd1ae6a3cdeb62e13ad4f9f902e66141e
Bisio98 <alberto.bisetti@hotmail.it>
2022-01-26 16:14:31 +0100

    milestone-2

--- index.php
+++ index.php
@@ -4,29 +4,27 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="css/style.css">
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/vue"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.20.0/axios.min.js"></script>
     <title>Dischi</title>
 </head>
 <body>
-    <header>
-        <img src="https://e1.pngegg.com/pngimages/252/890/png-clipart-spotify-for-os-x-el-capitan-spotify-logo.png" alt="Spotify Logo">
-    </header>
-    <main>
-        <?php
-        require __DIR__ . '/database.php';
-        ?>
-        <div class="container">
-
-            <?php foreach($database as $singleDisc){?>
-                <div class="single_card">
-                    <img src="<?php echo $singleDisc['poster']; ?>" alt="">
-                    <h2><?php echo $singleDisc['title']; ?></h2>
-                    <h3><?php echo $singleDisc['author']; ?></h3>
-                    <h4><?php echo $singleDisc['year']; ?></h4>
-                </div>
-            <?php } ?>
-            
-        </div> 
-    </main>
+    <div id="root">
+        <header>
+            <img src="https://e1.pngegg.com/pngimages/252/890/png-clipart-spotify-for-os-x-el-capitan-spotify-logo.png" alt="Spotify Logo">
+        </header>
+        <main>
+            <div class="container">
+                    <div v-for="element in discs" class="single_card">
+                        <img :src="element.poster" alt="">
+                        <h2>{{ element.title }}</h2>
+                        <h3>{{ element.author }}</h3>
+                        <h4>{{ element.year }}</h4>
+                    </div>                
+            </div> 
+        </main>
+    </div>
+    <script src="js/script.js"></script>
 </body>
 </html>
---
+++ server.php
@@ -0,0 +1,7 @@
+<?php
+    require __DIR__ .'/database.php';
+
+    $json = json_encode($database);
+    header('Content-Type: application/json');
+    echo $json;
+?>
